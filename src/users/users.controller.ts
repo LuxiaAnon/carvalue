4fb72538377be7c4e6f00069a5c00f4576f2a473
Nelly Borjon-Piron <60819573+LuxiaAnon@users.noreply.github.com>
2024-09-11 23:05:18 +0200

    nest interceptor

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,6 +7,9 @@ import {
   Post,
   Put,
   Query,
+  NotFoundException,
+  UseInterceptors,
+  ClassSerializerInterceptor,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
@@ -21,9 +24,14 @@ export class UsersController {
     this.userService.create(body.email, body.password);
   }
 
+  @UseInterceptors(ClassSerializerInterceptor)
   @Get('/:id')
-  findUser(@Param('id') id: string) {
-    return this.userService.findOne(Number(id));
+  async findUser(@Param('id') id: string) {
+    const user = await this.userService.findOne(Number(id));
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
   }
 
   @Get()
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
@@ -22,7 +22,7 @@ export class UsersService {
   async update(id: number, attrs: Partial<User>) {
     const user = await this.findOne(id);
     if (!user) {
-      throw new Error('User not find');
+      throw new NotFoundException("User not found");
     }
     Object.assign(user, attrs);
     return this.repo.save(user);
@@ -31,7 +31,7 @@ export class UsersService {
   async remove(id: number) {
     const user = await this.findOne(id);
     if (!user) {
-      throw new Error('User not find');
+      throw new NotFoundException("User not found");
     }
     return this.repo.remove(user);
   }
